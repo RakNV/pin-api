a8df3bed402779aefca0afb0870b49cd1c7371c8
RakNV <kinglink852@gmail.com>
2022-07-22 17:21:40 +0300

    moved declarative_base() to models.py

--- src/db.py
+++ src/db.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 
 DATABASE_URL = "postgresql://postgres:610700@localhost/pin"
 

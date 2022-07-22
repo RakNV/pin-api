ed0fd98d56cbd86b0bbf49e4d64e0a283db7ebd8
RakNV <kinglink852@gmail.com>
2022-07-22 16:14:48 +0300

    moved declarative_base() to models.py

--- src/db.py
+++ src/db.py
@@ -7,4 +7,3 @@ DATABASE_URL = "postgresql://postgres:610700@localhost/pin"
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
